90011900ee872218d44b365017cdabff2d57b8d7
Isabella Skořepová <isabella@skorepova.info>
2020-09-22 18:16:58 +0200

    simple midi demo

---
+++ pages/midi.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useRef, useState } from 'react'
+
+function useMidiSupported() {
+  const [v, setV] = useState<boolean | null>(null)
+  useEffect(() => {
+    setV(!!(navigator as any).requestMIDIAccess)
+  }, [])
+  return v
+}
+
+export default function MiDi() {
+  const [devices, setDevices] = useState({
+    midiIn: [] as any[],
+    midiOut: [] as any[],
+  })
+  const supported = useMidiSupported()
+
+  console.log(devices)
+
+  return (
+    <div>
+      <MidiListener
+        onStateChange={(midi) => {
+          setDevices({
+            midiIn: Array.from(midi.inputs.values()),
+            midiOut: Array.from(midi.outputs.values()),
+          })
+        }}
+      />
+      {supported
+        ? 'Your browser supports WebMiDi'
+        : supported === false
+        ? 'WebMiDi not supported on your browser'
+        : 'Loading...'}
+      <pre>
+        <code>{JSON.stringify(devices, null, 2)}</code>
+      </pre>
+      {devices.midiIn.map((device, key) => (
+        <MidiInputDevice key={key} device={device} />
+      ))}
+    </div>
+  )
+}
+
+function MidiInputDevice({ device }: { device: any }) {
+  const [notesOn, setNotesOn] = useState([])
+  useEffect(() => {
+    const notesOn = new Map()
+    function midiMessageReceived(event: any) {
+      // MIDI commands we care about. See
+      // http://webaudio.github.io/web-midi-api/#a-simple-monophonic-sine-wave-midi-synthesizer.
+      const NOTE_ON = 9
+      const NOTE_OFF = 8
+
+      const cmd = event.data[0] >> 4
+      const pitch = event.data[1]
+      const velocity = event.data.length > 2 ? event.data[2] : 1
+
+      // You can use the timestamp to figure out the duration of each note.
+      const timestamp = Date.now()
+
+      // Note that not all MIDI controllers send a separate NOTE_OFF command for every NOTE_ON.
+      if (cmd === NOTE_OFF || (cmd === NOTE_ON && velocity === 0)) {
+        setNotesOn((prev) => prev.filter((p) => p !== pitch))
+        console.log(
+          `🎧 from ${event.srcElement.name} note off: pitch:${pitch}, velocity: ${velocity}`,
+        )
+
+        // Complete the note!
+        const note = notesOn.get(pitch)
+        if (note) {
+          console.log(`🎵 pitch:${pitch}, duration:${timestamp - note} ms.`)
+          notesOn.delete(pitch)
+        }
+      } else if (cmd === NOTE_ON) {
+        setNotesOn((prev) => prev.filter((p) => p !== pitch).concat(pitch))
+        console.log(
+          `🎧 from ${event.srcElement.name} note off: pitch:${pitch}, velocity: {velocity}`,
+        )
+
+        // One note can only be on at once.
+        notesOn.set(pitch, timestamp)
+      }
+    }
+    device.addEventListener('midimessage', midiMessageReceived)
+    return () => {
+      device.removeEventListener('midimessage', midiMessageReceived)
+    }
+  }, [device])
+  return <div>{JSON.stringify(notesOn)}</div>
+}
+
+function MidiListener({
+  onStateChange,
+}: {
+  onStateChange: (midi: any) => void
+}) {
+  const [midi, setMidi] = useState<any>(null)
+  useEffect(() => {
+    ;(navigator as any).requestMIDIAccess().then(setMidi)
+  }, [])
+
+  const ref = useRef(onStateChange)
+  useEffect(() => {
+    ref.current = onStateChange
+  })
+
+  useEffect(() => {
+    if (!midi) return
+    const handleStateChange = (event: any) => {
+      ref.current?.(event.target)
+    }
+    ref.current?.(midi)
+    midi?.addEventListener('statechange', handleStateChange)
+    return () => {
+      midi?.removeEventListener('statechange', handleStateChange)
+    }
+  }, [midi])
+
+  return null
+}
