b072b195592470d213ac9119b38fc73ea800fed0
Isabella Skořepová <isabella@skorepova.info>
2020-09-29 13:11:39 +0200

    add some links

--- pages/rozvrh.tsx
+++ pages/rozvrh.tsx
@@ -2,13 +2,19 @@
 import { jsx } from '@emotion/react'
 import styled from '@emotion/styled'
 import { useRouter } from 'next/dist/client/router'
-import { ReactNode, useCallback } from 'react'
+import { ReactNode, useCallback, useEffect, useMemo, useState } from 'react'
 import { CalEvent, Cz, En, EventProvider } from '../components/calendar-event'
 
 export default function FEL() {
   const [showTitles, setShowTitles] = useBooleanQueryParam('titles', false)
   const [english, toggleLang] = useEn()
   const [deets, setDeets] = useBooleanQueryParam('deets', false)
+  const [links, setLinks] = useLocalStorage('rozvrh:links')
+
+  useSetOnWindow('setLink', (type: string, value: string) => {
+    setLinks({ ...links, [type]: value })
+  })
+
   return (
     <EventProvider
       value={{
@@ -86,10 +92,15 @@ export default function FEL() {
             <Cz>Úterý/</Cz>Tuesday
           </DayTitle>
           <CalEvent type="lecture" title="GPU" time="11:00 - 12:30">
-            Zoom
+            <MaybeLink to={links?.GPU}>Zoom</MaybeLink>
+            <br />
+            <Link to="https://cent.felk.cvut.cz/courses/GPU/index.html">
+              <En>Website</En>
+              <Cz>Stránky</Cz>
+            </Link>
           </CalEvent>
           <CalEvent type="seminar" title="GPU" time="14:30 - 16:00">
-            Zoom
+            <MaybeLink to={links?.GPU}>Zoom</MaybeLink>
           </CalEvent>
         </Day>
         <Day>
@@ -105,10 +116,14 @@ export default function FEL() {
             <Cz>Čtvrtek/</Cz>Wednesday
           </DayTitle>
           <CalEvent type="lecture" title="VG" time="9:15 - 10:45">
-            Zoom
+            <MaybeLink to={links?.CG}>Zoom</MaybeLink>
+            <br />
+            <Link to="https://cw.fel.cvut.cz/wiki/courses/cg/start">
+              CourseWare
+            </Link>
           </CalEvent>
           <CalEvent type="seminar" title="VG" time="11:00 - 12:30">
-            Zoom
+            <MaybeLink to={links?.CG}>Zoom</MaybeLink>
           </CalEvent>
           <CalEvent type="lecture" title="MMA" time="12:45 - 14:15">
             Teams
@@ -207,8 +222,76 @@ function useBooleanQueryParam(param: string, def: boolean) {
 
 function Link({ to, children }: { to: string; children: ReactNode }) {
   return (
-    <a href={to} target="_blank" rel="noopener noreferrer">
+    <a
+      href={to}
+      target="_blank"
+      rel="noopener noreferrer"
+      css={{ color: 'black' }}
+    >
       {children}
     </a>
   )
+}
+
+function MaybeLink({
+  to,
+  children,
+}: {
+  to: string | null
+  children: React.ReactNode
+}) {
+  if (!to) return <>{children}</>
+  return <Link to={to}>{children}</Link>
+}
+
+function useSetOnWindow(name: string, value: unknown) {
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const anyWindow = typeof window !== 'undefined' ? (window as any) : null
+    if (!anyWindow) return
+    const prev = anyWindow[name]
+    anyWindow[name] = value
+    return () => {
+      if (!prev) delete anyWindow[name]
+      else anyWindow[name] = prev
+    }
+  }, [name, value])
+}
+
+function safeParse(v: string | null) {
+  if (v === null) return null
+  try {
+    return JSON.parse(v)
+  } catch {
+    return null
+  }
+}
+
+let isSSR = true
+
+function useLocalStorage(key: string) {
+  const [value, setValue] = useState(() => {
+    if (isSSR) return null
+    return localStorage.getItem(key) ?? null
+  })
+  useEffect(() => {
+    setValue((v) => (v === null ? localStorage.getItem(key) ?? null : v))
+    isSSR = false
+    window.addEventListener('storage', (event) => {
+      if (event.key !== key) return
+      setValue(event.newValue)
+    })
+  }, [key])
+  return [
+    useMemo(() => safeParse(value), [value]),
+    useCallback(
+      (value: unknown) => {
+        const json = value === null ? null : JSON.stringify(value)
+        if (json === null) localStorage.removeItem(key)
+        else localStorage.setItem(key, json)
+        setValue(json)
+      },
+      [key],
+    ),
+  ] as const
 }
