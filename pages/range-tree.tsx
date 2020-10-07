08c6859b7ad42099d08325b68e353a581b76f5c4
Isabella Skořepová <isabella@skorepova.info>
2020-10-07 17:28:32 +0200

    highlights

--- pages/range-tree.tsx
+++ pages/range-tree.tsx
@@ -1,32 +1,248 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react'
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { Fragment, useEffect, useMemo, useRef, useState } from 'react'
 import Resizer from '@codewitchbella/react-resizer'
 
-export default function RangeTreeE() {
-  const [key, setKey] = useState(0)
+type Point = { x: number; y: number; id: number }
+type Points = readonly Point[]
+type Highlight = {
+  layer: number
+  id: number
+  ymin: number
+  path: readonly ('left' | 'right')[]
+}
+export default function RangeTree() {
+  const [points, setPoints] = useState<Points>([])
+  const bbst = useMemo(() => makeBBST(points), [points])
+  const fractal = useMemo(() => makeFractal(points), [points])
+  const [highlight, setHighlight] = useState<Highlight>({
+    layer: 0,
+    id: 0,
+    ymin: 0,
+    path: [],
+  })
   return (
     <div>
-      <button type="button" onClick={() => setKey((i) => i + 1)}>
-        Reset
+      <button type="button" onClick={() => setPoints([])}>
+        Remove all points
       </button>
-      <RangeTree key={key} />
+      <PointInput
+        onPoint={(point) => {
+          setPoints((p) => [
+            ...p,
+            { ...point, id: p.reduce((b, { id }) => Math.max(id, b), 0) + 1 },
+          ])
+        }}
+      />
+      <button
+        type="button"
+        onClick={() => {
+          setPoints(
+            [
+              [3, 7],
+              [4, 6],
+              [6, 5],
+              [1, 4],
+              [5, 3],
+              [2, 2],
+              [7, 1],
+              [0, 0],
+            ].map(([x, y], i) => ({ x, y, id: i + 1 })),
+          )
+        }}
+      >
+        Load example
+      </button>
+      <PointChart points={points} />
+      <div>
+        <label>
+          ymin:{' '}
+          <input
+            type="number"
+            value={highlight.ymin}
+            onChange={(evt) => {
+              const next = Number.parseInt(evt.target?.value, 10)
+              if (Number.isInteger(next))
+                setHighlight((h) => ({
+                  ...h,
+                  ymin: next,
+                }))
+            }}
+          />
+        </label>
+        <button
+          type="button"
+          onClick={() => {
+            setHighlight((h) => ({
+              layer: h.layer + 1,
+              id: h.id * 2,
+              ymin: h.ymin,
+              path: [...h.path, 'left'],
+            }))
+          }}
+        >
+          Left
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setHighlight((h) => ({
+              layer: h.layer + 1,
+              id: h.id * 2 + 1,
+              ymin: h.ymin,
+              path: [...h.path, 'right'],
+            }))
+          }}
+        >
+          Right
+        </button>
+      </div>
+      <div css={{ display: 'flex' }}>
+        <BBSTView bbst={bbst} highlight={highlight} />
+        <Fractal fractal={fractal} highlight={highlight} />
+      </div>
     </div>
   )
 }
 
-type Point = { x: number; y: number }
-type Points = readonly Point[]
-function RangeTree() {
-  const [points, setPoints] = useState<Points>([])
-  const bbst = useMemo(() => makeBBST(points), [points])
+// eslint-disable-next-line @typescript-eslint/ban-types
+type FractalNode<Ext = {}> = Ext & {
+  left: FractalNode<Ext> | null
+  right: FractalNode<Ext> | null
+  sibRight: FractalNode<Ext> | null
+  value: number
+}
+
+function makeFractal(points: Points) {
+  type FractalNodeWork = FractalNode<{ x: number; id: number }>
+  let layer = [...points]
+    .sort(({ x: a }, { x: b }) => a - b)
+    .map(
+      (point): FractalNodeWork => ({
+        x: point.x,
+        value: point.y,
+        id: point.id,
+        left: null,
+        right: null,
+        sibRight: null,
+      }),
+    )
+
+  const layers: FractalNode[][] = [layer]
+  while (layer.length > 1) {
+    const newLayer: FractalNodeWork[] = []
+    for (let i = 0; i < layer.length; i += 2) {
+      const left = layer[i]
+      const right: FractalNodeWork | null = layer[i + 1] ?? null
+      const nodes = [...getFractalValues(left), ...getFractalValues(right)]
+        .sort(({ value: a }, { value: b }) => a - b)
+        .map(
+          (node, i, list): FractalNodeWork => ({
+            ...node,
+            // this could be more optimal but :shrug:
+            left: findFirstSameOrLarger(left, node.value),
+            right: findFirstSameOrLarger(right, node.value),
+          }),
+        )
+      let j = 0
+      for (const node of nodes) {
+        node.sibRight = nodes[++j] ?? null
+      }
+      newLayer.push(nodes[0])
+    }
+    layers.push(newLayer)
+    layer = newLayer
+  }
+  for (const l of layers) {
+    for (const n of l) {
+      delete (n as any).x
+    }
+  }
+  return { layers: layers.reverse(), root: layer[0] }
+}
+
+function Fractal({
+  fractal,
+  highlight,
+}: {
+  fractal: ReturnType<typeof makeFractal>
+  highlight: Highlight
+}) {
+  const highlightedNode = findHighlightedNode(fractal, highlight)
   return (
-    <div>
-      <PointInput onPoint={(point) => setPoints((p) => [...p, point])} />
-      <PointChart points={points} />
-      <BBSTView bbst={bbst} />
+    <div
+      css={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch' }}
+    >
+      {fractal.layers.map((layer, layerKey) => (
+        <div
+          key={layerKey}
+          css={{
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            gap: '.25rem',
+          }}
+        >
+          {layer.map((layerStart, ni) => (
+            <div key={ni}>
+              [
+              {getFractalNodes(layerStart).map((node, i, list) => (
+                <>
+                  <span
+                    css={
+                      node === highlightedNode ? { background: 'yellow' } : {}
+                    }
+                  >
+                    {node.value}
+                  </span>
+                  {i !== list.length - 1 ? ',' : null}
+                </>
+              ))}
+              ]
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   )
+}
+function findHighlightedNode(
+  fractal: ReturnType<typeof makeFractal>,
+  highlight: Highlight,
+): FractalNode | null {
+  let start: FractalNode | null = fractal.root
+  while (start && start.value < highlight.ymin) {
+    start = start.sibRight
+  }
+  if (!start) return null
+
+  return highlight.path.reduce<FractalNode | null>(
+    (v, cur) => v?.[cur] ?? null,
+    start,
+  )
+}
+
+function findFirstSameOrLarger<T>(node: FractalNode<T>, value: number) {
+  for (let n: FractalNode<T> | null = node; n; n = n.sibRight) {
+    if (n.value >= value) return n
+  }
+  return null
+}
+
+function getFractalValues<T>(a: FractalNode<T> | null): FractalNode<T>[] {
+  return getFractalNodes(a).map((node) => ({
+    ...node,
+    left: null,
+    right: null,
+    sibRight: null,
+  }))
+}
+
+function getFractalNodes<T>(a: FractalNode<T> | null): FractalNode<T>[] {
+  const nodes: FractalNode<T>[] = []
+  for (let node: FractalNode<T> | null = a; node; node = node.sibRight) {
+    nodes.push(node)
+  }
+  return nodes
 }
 
 type BBSTNode = { value: number; left: BBSTNode | null; right: BBSTNode | null }
@@ -58,7 +274,13 @@ function findMax(node: BBSTNode | null): number | null {
   )
 }
 
-function BBSTView(props: { bbst: ReturnType<typeof makeBBST> }) {
+function BBSTView({
+  bbst,
+  highlight,
+}: {
+  bbst: ReturnType<typeof makeBBST>
+  highlight: Highlight
+}) {
   return (
     <div css={{ display: 'flex' }}>
       <div
@@ -68,7 +290,7 @@ function BBSTView(props: { bbst: ReturnType<typeof makeBBST> }) {
           alignItems: 'stretch',
         }}
       >
-        {props.bbst.layers.map((layer, li) => (
+        {bbst.layers.map((layer, li) => (
           <div
             key={li}
             css={{
@@ -79,7 +301,16 @@ function BBSTView(props: { bbst: ReturnType<typeof makeBBST> }) {
           >
             <span />
             {layer.map((node, ni) => (
-              <span key={ni}>{node.value}</span>
+              <span
+                key={ni}
+                css={
+                  highlight.layer === li && highlight.id === ni
+                    ? { background: 'yellow' }
+                    : {}
+                }
+              >
+                {node.value}
+              </span>
             ))}
             <span />
           </div>
@@ -141,7 +372,6 @@ function PointChart({ points }: { points: Points }) {
               bb: evt.currentTarget.getBoundingClientRect(),
             }
           const { bb } = bbRef.current
-          console.log(evt.currentTarget.clientTop)
           const x = evt.clientX - bb.left + window.scrollX
           const y = evt.clientY - bb.top + window.scrollY
           const l = label.current
@@ -203,7 +433,11 @@ function PointChart({ points }: { points: Points }) {
   )
 }
 
-function PointInput({ onPoint }: { onPoint: (point: Point) => void }) {
+function PointInput({
+  onPoint,
+}: {
+  onPoint: (point: { x: number; y: number }) => void
+}) {
   return (
     <form
       onSubmit={(evt) => {
