ed7337673e33ff41f6f10b5651028139acbd6ceb
Isabella Skořepová <isabella@skorepova.info>
2021-08-19 13:33:03 +0200

    /soukromi

---
+++ pages/soukromi.tsx
@@ -0,0 +1,15 @@
+import { Technical } from '../components/technical'
+import type { PropsWithChildren } from 'react'
+
+export default function Index() {
+  return (
+    <>
+      <style jsx>{``}</style>
+      <h1>Zásady ochrany soukromí</h1>
+      <p>
+        Moje zásady ochrany soukromí pro aplikaci Snek jsou velmi jednoduché:
+        nesbírám žádná data.
+      </p>
+    </>
+  )
+}
